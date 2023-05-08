bded9cf2a21370272694e8b7fa5a6eae3bd3db86
Snir Bachar <snir@quantum-machines.co>
2023-05-08 23:47:22 +0300

    Add BFS and Greedy agents

--- Algorithms.py
+++ Algorithms.py
@@ -14,12 +14,26 @@ class BFSAgent():
 
 
 class DFSAgent():
-    def __init__(self) -> None:
-        raise NotImplementedError
+    def __init__(self):
+        return
 
-    def search(self, env: FrozenLakeEnv) -> Tuple[List[int], int, float]:
-        raise NotImplementedError
-        
+    def search(self, env: FrozenLakeEnv) -> Tuple[List[int], int, set[int]]:
+        def dfs(state, path, cost, explored):
+            if env.is_final_state(state):
+                return path, cost, len(explored)
+            for action in range(env.action_space.n):
+                env.set_state(state)
+                next_state, action_cost, terminated = env.step(action)
+                if action_cost == np.inf:
+                    explored.add(next_state)
+                    continue
+                if next_state not in explored:
+                    result = dfs(next_state, path + [action], cost + action_cost, explored | {next_state})
+                    if result[0]:
+                        return result
+            return [], np.inf, 0.
+        return dfs(env.reset(), [env.reset()], 0, set())
+
 
 
 class UCSAgent():
@@ -33,12 +47,11 @@ class UCSAgent():
 
 
 class GreedyAgent():
-  
-    def __init__(self) -> None:
-        raise NotImplementedError
+    def __init__(self, h):
+        self.h = h
 
     def search(self, env: FrozenLakeEnv) -> Tuple[List[int], int, float]:
-        raise NotImplementedError
+
 
 class WeightedAStarAgent():
     
