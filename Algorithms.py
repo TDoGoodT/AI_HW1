889f25cd4741a4af0929e8c122cb2baa5b844d13
Snir Bachar <snir@quantum-machines.co>
2023-05-09 15:41:59 +0300

    Finish GreedyAgent, add test

--- Algorithms.py
+++ Algorithms.py
@@ -1,8 +1,10 @@
+from queue import PriorityQueue
 import numpy as np
 
 from FrozenLakeEnv import FrozenLakeEnv
 from typing import List, Tuple
 import heapdict
+
 BOARD_SIZE = 8
 
 class BFSAgent():
@@ -45,35 +47,61 @@ class UCSAgent():
         raise NotImplementedError
 
 def h_manhattan(state: int, goal: int):
-    return np.abs(state // BOARD_SIZE - goal // BOARD_SIZE) + np.abs(state % BOARD_SIZE - goal % BOARD_SIZE)
+    y_distance = np.abs(state // BOARD_SIZE - goal // BOARD_SIZE)
+    x_distance = np.abs(state % BOARD_SIZE - goal % BOARD_SIZE)
+    return y_distance + x_distance
 
 def h_sap(state: int, goal: int, acc_cost: int):
     return min(h_manhattan(state, goal), acc_cost)
 
+
+def step(state, action, env):
+    env.set_state(state)
+    return env.step(action)
 class GreedyAgent():
     def __init__(self):
-        self.h = h_sap
+        self.heuristic_fn = h_sap
 
     def search(self, env: FrozenLakeEnv) -> Tuple[List[int], int, set[int]]:
-        frontier = [(env.reset(), 0)]
-        explored = set()
-        while frontier:
-            state, cost = frontier.pop(0)
-            if state in explored:
-                continue
-            explored.add(state)
-            for action in range(env.action_space.n):
-                env.set_state(state)
-                next_state, action_cost, terminated = env.step(action)
-                if terminated and action_cost == np.inf:
-                    explored.add(next_state)
-                    continue
-                if next_state not in explored:
-                    smallest_h = min([self.h(next_state, goal, cost + action_cost) for goal in env.get_goal_states()])
-                    frontier.append((next_state, cost + action_cost + smallest_h))
-            frontier.sort(key=lambda x: x[1])
-        return [], np.inf, []
+        def greedy_search(graph, start, goal):
+            # Create an empty priority queue and insert the start node
+            pq = PriorityQueue()
+            pq.put((0, start))
 
+            # Initialize the visited set and parent dictionary
+            visited = set()
+            parent = {start: None}
+
+            # Continue searching until the priority queue is empty
+            while not pq.empty():
+                # Get the node with the lowest priority
+                current_cost, current = pq.get()
+
+                # Check if we've reached the goal
+                if current == goal:
+                    path = []
+                    while current is not None:
+                        path.append(current)
+                        current = parent[current]
+                    path.reverse()
+                    return path, current_cost, set()
+
+                # Mark the current node as visited
+                visited.add(current)
+
+                # Check the neighbors of the current node
+                for neighbor, cost, terminated in [step(current, action, env) for action in range(env.action_space.n)]:
+                    # If the neighbor has not been visited, add it to the priority queue
+                    if neighbor not in visited:
+                        priority = self.heuristic_fn(neighbor, goal, cost) # Calculate the priority using a heuristic function
+                        pq.put((current_cost + cost + priority, neighbor))
+                        visited.add(neighbor)
+                        parent[neighbor] = current
+
+            # If we couldn't find a path to the goal, return None
+            return [], np.inf, set()
+
+        return greedy_search({env.reset(): []}, 0, env.get_goal_states()[0])
 class WeightedAStarAgent():
     
     def __init__(self):
---
+++ GreedyTests.py
@@ -0,0 +1,97 @@
+MAP = [
+        "SFFFFFFF",
+        "FFFFFTAL",
+        "TFFHFFTF",
+        "FPFFFHTF",
+        "FAFHFPFF",
+        "FHHFFFHF",
+        "FHTFHFTL",
+        "FLFHFFFG",
+    ]
+BOARD_SIZE = 8
+import time
+from IPython.display import clear_output
+import numpy as np
+from Algorithms import GreedyAgent, h_manhattan, h_sap
+from FrozenLakeEnv import FrozenLakeEnv
+env = FrozenLakeEnv(MAP)
+
+def print_solution(actions,env: FrozenLakeEnv) -> None:
+    env.reset()
+    total_cost = 0
+    print(env.render())
+    print(f"Timestep: {1}")
+    print(f"State: {env.get_state()}")
+    print(f"Action: {None}")
+    print(f"Cost: {0}")
+    time.sleep(1)
+
+    for i, action in enumerate(actions):
+      state, cost, terminated = env.step(action)
+      total_cost += cost
+      clear_output(wait=True)
+
+      print(env.render())
+      print(f"Timestep: {i + 2}")
+      print(f"State: {state}")
+      print(f"Action: {action}")
+      print(f"Cost: {cost}")
+      print(f"Total cost: {total_cost}")
+      
+      time.sleep(1)
+
+      if terminated is True:
+        break
+
+def test_greedy_agent():
+    env.reset()
+    agent = GreedyAgent()
+    
+    path, cost, explored = agent.search(env)
+    print(f"Path: {path}, cost: {cost}, explored: {explored}")
+    
+    
+
+
+def test_h_manhattan():
+    # Test distance between two adjacent cells]
+    expected = 1
+    assert h_manhattan(0, 1) == expected, f"h_manhattan(0, 1) should be {expected}"
+    assert h_manhattan(1, 0) == expected, f"h_manhattan(1, 0) should be {expected}"
+    # Test distance between two cells in the same row
+    expected = 4
+    assert h_manhattan(7, 3) == expected, f"h_manhattan(7, 3) should be {expected}"
+    assert h_manhattan(3, 7) == expected, f"h_manhattan(3, 7) should be {expected}"
+    # Test distance between two cells in the same column
+    expected = 1
+    assert h_manhattan(4, 12) == expected, f"h_manhattan(4, 12) should be {expected}"
+    assert h_manhattan(12, 4) == expected, f"h_manhattan(12, 4) should be {expected}"
+    # Test distance between two cells in a diagonal
+    expected = 3
+    assert h_manhattan(0, 10) == expected, f"h_manhattan(0, 10) should be {expected}"
+    assert h_manhattan(10, 0) == expected, f"h_manhattan(10, 0) should be {expected}"
+
+def test_h_sap():
+    # Test when accumulated cost is 0
+    expected = 0
+    assert h_sap(0, 1, 0) == expected, f"h_sap(0, 1, 0) should be {expected}"
+    assert h_sap(7, 3, 0) == expected, f"h_sap(7, 3, 0) should be {expected}"
+    assert h_sap(4, 12, 0) == expected, f"h_sap(4, 12, 0) should be {expected}"
+    assert h_sap(0, 9, 0) == expected, f"h_sap(0, 9, 0) should be {expected}"
+    # Test when accumulated cost is non-zero
+    expected = 1
+    assert h_sap(0, 1, 9) == expected, f"h_sap(0, 1, 9) should be {expected}"
+    expected = 3
+    assert h_sap(7, 4, 5) == expected, f"h_sap(7, 4, 5) should be {expected}"
+    expected = 5
+    assert h_sap(16, 56, 20) == expected, f"h_sap(16, 56, 20) should be {expected}"
+    expected = 2
+    assert h_sap(0, 9, 15) == expected, f"h_sap(0, 9, 15) should be {expected}"
+
+def main():
+    test_greedy_agent()
+    test_h_manhattan()
+    test_h_sap()
+    
+if __name__ == "__main__":
+    main()
